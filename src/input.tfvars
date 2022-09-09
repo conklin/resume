8672e685bd94b3742d179869cf3c930a2eb37545
mconklin <ssi.conklin@gmail.com>
2022-09-09 09:42:55 -0400

    thought it would be possible to create a domain and manage the dns records, however there looks to be no support at this time for the gcp provider

--- src/input.tfvars
+++
@@ -1,3 +0,0 @@
-project_id = "mconklin"
-compute_region="us-east1"
-compute_zone="us-east1-b"
--- src/main.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project = var.project_id
-  region = var.compute_region
-  zone = var.compute_zone
-}
